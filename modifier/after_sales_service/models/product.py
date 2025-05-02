7f89343c127bda65f26bf2470f20220b5d212ca5
Ridho Kurnia <dhoridhokp@gmail.com>
2025-05-02 19:00:28 +0700

    fillestore up

--- modifier/after_sales_service/models/product.py
+++ modifier/after_sales_service/models/product.py
@@ -1,5 +1,6 @@
 from odoo import models, fields, api
 from odoo.tools.translate import html_translate
+from lxml import html
 
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
@@ -7,6 +8,21 @@ class ProductTemplate(models.Model):
     has_warranty = fields.Boolean(string='Has Warranty')
     warranty_period_id = fields.Many2one('warranty.period', string='Warranty Period')
     website_description = fields.Html('Description for the website', sanitize_attributes=False, translate=html_translate, sanitize_form=False)
+
+    def action_update_warranty_description(self):
+        for product in self:
+            name = ''
+            desc = ''
+            if product.warranty_period_id:
+                name = product.warranty_period_id.name or ''
+            desc = product.description or ''
+            description_html = f"<p><strong>Warranty Coverage:</strong> {name}</p> <p>{desc}</p>"
+            product.website_description = description_html
+
+    @api.onchange('warranty_period_id', 'description')
+    def _onchange_for_website_description(self):
+        for record in self:
+            record.action_update_warranty_description()
 
     @api.onchange('has_warranty')
     def _onchange_product_has_warranty(self):
